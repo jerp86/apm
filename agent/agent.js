04e0e4f0c87a7123cd5830bf67ceaca00dff4ac5
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-10-18 22:59:21 -0300

    create agent

---
+++ agent/agent.js
@@ -0,0 +1,28 @@
+import Http from "http";
+import debug from "debug";
+import { randomUUID } from "crypto";
+
+const log = debug("agent:runner");
+
+function start(db) {
+  const emit = Http.Server.prototype.emit;
+  Http.Server.prototype.emit = function (type, req, res) {
+    if (type !== "request") {
+      return emit.apply(this, arguments);
+    }
+
+    const customerId = req.headers["x-app-id"];
+    const customer = db.find(
+      (customer) => customer.id === parseInt(customerId)
+    );
+    const data = { customerId, ...customer, requestId: randomUUID() };
+
+    res.setHeader("x-request-id", data.requestId);
+    req.user = data;
+    log("a request has happened!", req.headers);
+
+    return emit.apply(this, arguments);
+  };
+}
+
+export { start };
