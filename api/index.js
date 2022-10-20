672dee3e80bb937e62902be8748283f7f3c44ae5
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-10-20 17:33:56 -0300

    add mapData

--- api/index.js
+++ api/index.js
@@ -3,11 +3,46 @@ const log = debug("app:api");
 import Http from "http";
 
 import database from "../database.json";
+
 import { start } from "../agent/agent.js";
 start(database);
 
+import { pipeline, Transform } from "stream";
+import { promisify } from "util";
+
+const pipelineAsync = promisify(pipeline);
+
+const mapData = (lang) =>
+  new Transform({
+    destroy(error) {
+      log("called autodestroy!!");
+    },
+    transform: (chunk, encoding, cb) => {
+      const data = JSON.parse(chunk);
+
+      const languageNames = new Intl.DisplayNames([lang], { type: "currency" });
+
+      const result = JSON.stringify({
+        name: data.name,
+        currency: languageNames.of(data.currency),
+        preferences: data.preferences?.description ?? "not found",
+      });
+
+      cb(null, result);
+    },
+  });
+
 const startServer = async (req, res) => {
-  return res.end("Hey dude!");
+  if (req.method !== "POST") {
+    return res.end("Hey dude!");
+  }
+
+  try {
+    await pipelineAsync(req, mapData(req.user.speaks), res);
+  } catch (error) {
+    log("Internal server error!", error.message);
+    return res.end("Internal server error");
+  }
 };
 
 const port = 3000;
